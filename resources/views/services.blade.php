aa3e469026f89f5d4a0caf10be86f1da9001a650
RodrigoDeNA <rodrigovich619@gmail.com>
2026-01-09 14:39:09 +0100

    Trabajo en los componentes .blade, de mi menu.

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,4 +2,16 @@
 @section('title', 'Services')
 @section('body')
     <h1> Cuerpo del servicios </h1>
+    @component('_components.card')
+        @slot('title', 'Serivice 1')
+        @slot('content', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi voluptate perspiciatis suscipit exercitationem voluptatibus repudiandae. Vero, cupiditate at. Non suscipit natus magni aliquam harum debitis, ipsam sapiente doloremque nam rem.')
+    @endcomponent
+
+     @component('_components.card')
+        @slot('title', 'Serivice 2')
+        @slot('content')
+            <h3>Example</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+        @endslot
+    @endcomponent
 @endsection
